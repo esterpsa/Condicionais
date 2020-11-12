8d9a843cba1edfb30a767afb32bd3ec7f0a637c4
Ester <ester.albuquerque500@gmail.com>
2020-11-12 10:17:02 -0300

    condicionais - media

---
+++ condiciobas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace condiciobas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        //declarar as variaveis 
+        float nota1, nota2, nota3, media;
+        string nomeCompleto;
+
+
+        //entrada de dados
+        Console.WriteLine("Digite o nome do aluno");
+        nomeCompleto = Console.ReadLine();
+        
+        Console.Write("Digite a primeira nota: ");
+        nota1 = float.Parse(Console.ReadLine());
+
+        Console.Write("Digite a primeira nota: ");
+        nota2 = float.Parse(Console.ReadLine());
+
+        Console.Write("Digite a primeira nota: ");
+        nota3 = float.Parse(Console.ReadLine());
+
+        //processamento 
+
+        media = (nota1+nota2+3/3/3);
+
+        //exibir resultado
+        if(media>5){
+            //valor verdadeiro
+            Console.WriteLine("O aluno "+nomeCompleto+" está aprovado com a média de: "+media);
+        }else{
+            //valor valor
+            Console.WriteLine("O aluno "+nomeCompleto+" está reprovado com a media de: "+media);
+        }
+
+
+
+        }
+    }
+}
